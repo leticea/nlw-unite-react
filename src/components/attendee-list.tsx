5f48dce500a4b7f04a5d2ad2080a167b39abfa78
leticea <nascimento.leticeah@gmail.com>
2024-06-26 11:58:55 -0300

    feat(attendee-list.tsx): creating styles

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -18,52 +18,63 @@ export function AttendeeList() {
         <table className="w-full">
           <thead>
             <tr className="border-b border-white/10">
-              <th className="py-3 px-2.5 text-sm font-semibold text-left">
+              <th className="py-3 px-4 text-sm font-semibold text-left">
                 <input type="checkbox" />
               </th>
-              <th className="py-3 px-2.5 text-sm font-semibold text-left">
+              <th className="py-3 px-4 text-sm font-semibold text-left">
                 Código
               </th>
-              <th className="py-3 px-2.5 text-sm font-semibold text-left">
+              <th className="py-3 px-4 text-sm font-semibold text-left">
                 Participante
               </th>
-              <th className="py-3 px-2.5 text-sm font-semibold text-left">
+              <th className="py-3 px-4 text-sm font-semibold text-left">
                 Data de inscrição
               </th>
-              <th className="py-3 px-2.5 text-sm font-semibold text-left">
+              <th className="py-3 px-4 text-sm font-semibold text-left">
                 Data do check-in
               </th>
-              <th className="py-3 px-2.5 text-sm font-semibold text-left"></th>
+              <th className="py-3 px-4 text-sm font-semibold text-left"></th>
             </tr>
           </thead>
           <tbody>
-            <tr className="border-b border-white/10">
-              <td className="py-3 px-2.5 text-sm text-zinc-300">
-                <input type="checkbox" />
-              </td>
-              <td className="py-3 px-2.5 text-sm text-zinc-300">12383</td>
-              <td className="py-3 px-2.5 text-sm text-zinc-300">
-                <div className="flex flex-col gap-1">
-                  <span className="font-semibold text-white">
-                    Letícia Mangueira
-                  </span>
+            {Array.from({ length: 10 }).map((_, i) => {
+              return (
+                <tr key={i} className="border-b border-white/10">
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    <input type="checkbox" />
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">12383</td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    <div className="flex flex-col gap-1">
+                      <span className="font-semibold text-white">
+                        Letícia Mangueira
+                      </span>
 
-                  <span>leticia@mangueira.com.br</span>
-                </div>
-              </td>
-              <td className="py-3 px-2.5 text-sm text-zinc-300">
-                7 dias atrás
-              </td>
-              <td className="py-3 px-2.5 text-sm text-zinc-300">
-                3 dias atrás
-              </td>
-              <td className="py-3 px-2.5 text-sm text-zinc-300"></td>
-            </tr>
+                      <span>leticia@mangueira.com.br</span>
+                    </div>
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    7 dias atrás
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    3 dias atrás
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300"></td>
+                </tr>
+              );
+            })}
           </tbody>
           <tfoot>
             <tr>
-              <td colSpan={3}>Mostrando 10 de 228</td>
-              <td colSpan={3}>Página 1 de 23</td>
+              <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
+                Mostrando 10 de 228
+              </td>
+              <td
+                className="py-3 px-4 text-sm text-zinc-300 text-right"
+                colSpan={3}
+              >
+                Página 1 de 23
+              </td>
             </tr>
           </tfoot>
         </table>
