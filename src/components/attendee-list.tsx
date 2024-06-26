c97794edc744ee92ff724fc1bf67fac98e7d54b7
leticea <nascimento.leticeah@gmail.com>
2024-06-26 15:53:14 -0300

    feat(icon-button.tsx):creating icon button component

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -6,12 +6,13 @@ import {
   MoreHorizontal,
   Search,
 } from "lucide-react";
+import { IconButton } from "./icon-button";
 
 export function AttendeeList() {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
-        <h1 className="text-2xl font-bold">Participantes</h1>
+        <h1 className="text-2xl font-bold">Participants</h1>
         <div className="px-3 py-1.5 w-72 border border-white/10 rounded-lg flex items-center gap-3">
           <Search className="size-4 text-emerald-300" />
           <input
@@ -35,16 +36,16 @@ export function AttendeeList() {
                 />
               </th>
               <th className="py-3 px-4 text-sm font-semibold text-left">
-                Código
+                Codes
               </th>
               <th className="py-3 px-4 text-sm font-semibold text-left">
-                Participante
+                Participant
               </th>
               <th className="py-3 px-4 text-sm font-semibold text-left">
-                Data de inscrição
+                Registration date
               </th>
               <th className="py-3 px-4 text-sm font-semibold text-left">
-                Data do check-in
+                Check-in date
               </th>
               <th
                 style={{ width: 64 }}
@@ -76,15 +77,15 @@ export function AttendeeList() {
                     </div>
                   </td>
                   <td className="py-3 px-4 text-sm text-zinc-300">
-                    7 dias atrás
+                    7 days ago
                   </td>
                   <td className="py-3 px-4 text-sm text-zinc-300">
-                    3 dias atrás
+                    3 days ago
                   </td>
                   <td className="py-3 px-4 text-sm text-zinc-300">
-                    <button className="bg-black/20 border border-white/10 rounded-md p-1.5">
+                    <IconButton transparent>
                       <MoreHorizontal className="size-4" />
-                    </button>
+                    </IconButton>
                   </td>
                 </tr>
               );
@@ -93,27 +94,27 @@ export function AttendeeList() {
           <tfoot>
             <tr>
               <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
-                Mostrando 10 de 228
+                Showing 10 of 228
               </td>
               <td
                 className="py-3 px-4 text-sm text-zinc-300 text-right"
                 colSpan={3}
               >
                 <div className="inline-flex items-center gap-8">
-                  <span>Página 1 de 23</span>
+                  <span>Page 1 of 23</span>
                   <div className="flex gap-1.5">
-                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                    <IconButton>
                       <ChevronsLeft className="size-4" />
-                    </button>
-                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                    </IconButton>
+                    <IconButton>
                       <ChevronLeft className="size-4" />
-                    </button>
-                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                    </IconButton>
+                    <IconButton>
                       <ChevronRight className="size-4" />
-                    </button>
-                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                    </IconButton>
+                    <IconButton>
                       <ChevronsRight className="size-4" />
-                    </button>
+                    </IconButton>
                   </div>
                 </div>
               </td>
