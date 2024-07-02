e31a9fc62386d77f61db8d6e0249935a53808c09
leticea <nascimento.leticeah@gmail.com>
2024-07-02 16:53:43 -0300

    feat(readme): creating readme file

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,6 @@ export function Header() {
   return (
     <div className="flex items-center gap-5 py-2">
       <img src={nlwUniteIcon} />
-
       <nav className="flex items-center gap-5">
         <NavLink className="text-zinc-300" href="/eventos">
           Events
